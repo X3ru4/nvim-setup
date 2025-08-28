be3ea1b329452b36fac20d38f350bc0488986f40
X3ru4 <phammaitrunghuyv3@gmail.com>
2025-08-28 17:01:04 +0700

    recomment than recomment

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -2,8 +2,24 @@ return {
 	"saghen/blink.cmp",
 	event = "InsertEnter",
 	dependencies = {
-		{ "rafamadriz/friendly-snippets" },
 		{ "onsails/lspkind.nvim" },
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			dependencies = { "rafamadriz/friendly-snippets" },
+			config = function()
+				require("luasnip").setup()
+				require("luasnip.loaders.from_vscode").lazy_load()
+
+				local ls = require("luasnip")
+				vim.keymap.set({ "i", "s" }, "<tab>", function()
+					ls.jump(1)
+				end, { silent = true })
+				vim.keymap.set({ "i", "s" }, "<c-l>", function()
+					ls.jump(-1)
+				end, { silent = true })
+			end,
+		},
 		{
 			"folke/lazydev.nvim",
 			ft = "lua",
@@ -65,36 +81,13 @@ return {
 							label = {
 								width = { fill = true, max = 30 },
 							},
-							kind_icon = {
-								text = function(ctx)
-									local icon = ctx.kind_icon
-									if vim.tbl_contains({ "Path" }, ctx.source_name) then
-										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-										if dev_icon then
-											icon = dev_icon
-										end
-									else
-										icon = require("lspkind").symbolic(ctx.kind, {
-											mode = "symbol",
-										})
-									end
-									return icon .. ctx.icon_gap
-								end,
-								highlight = function(ctx)
-									local hl = ctx.kind_hl
-									if vim.tbl_contains({ "Path" }, ctx.source_name) then
-										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-										if dev_icon then
-											hl = dev_hl
-										end
-									end
-									return hl
-								end,
-							},
 						},
 						treesitter = { "lsp" },
 					},
 				},
+			},
+			snippets = {
+				preset = "luasnip",
 			},
 			sources = {
 				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
