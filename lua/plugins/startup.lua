46c51d95c59c36a8edb815b5efec08be52d2a085
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-03-28 16:23:05 +0700

    v74: Better header in the startup

--- lua/plugins/startup.lua
+++ lua/plugins/startup.lua
@@ -17,38 +17,57 @@ return {
 			end
 			local header_style = {
 				{
-					"██╗░░██╗██████╗░██████╗░██╗░░░██╗░░██╗██╗",
-					"╚██╗██╔╝╚════██╗██╔══██╗██║░░░██║░██╔╝██║",
-					"░╚███╔╝░░█████╔╝██████╔╝██║░░░██║██╔╝░██║",
-					"░██╔██╗░░╚═══██╗██╔══██╗██║░░░██║███████║",
-					"██╔╝╚██╗██████╔╝██║░░██║╚██████╔╝╚════██║",
-					"╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝░╚═════╝░░░░░░╚═╝",
+					"     ▄   ▄",
+					" ▄█▄ █▀█▀█ ▄█▄",
+					"▀▀████▄█▄████▀▀",
+					"     ▀█▀█▀",
 				},
 				{
-					"╭━╮╭━┳━━━╮╱╱╱╱╭╮╱╭╮",
-					"╰╮╰╯╭┫╭━╮┃╱╱╱╱┃┃╱┃┃",
-					"╱╰╮╭╯╰╯╭╯┣━┳╮╭┫╰━╯┃",
-					"╱╭╯╰╮╭╮╰╮┃╭┫┃┃┣━━╮┃",
-					"╭╯╭╮╰┫╰━╯┃┃┃╰╯┃╱╱┃┃",
-					"╰━╯╰━┻━━━┻╯╰━━╯╱╱╰╯",
+					"  ▄    ▄▄▄▄▄▄▄    ▄   ",
+					" ▀▀▄ ▄█████████▄ ▄▀▀  ",
+					"     ██ ▀███▀ ██      ",
+					"   ▄ ▀████▀████▀ ▄    ",
+					" ▀█    ██▀█▀██    █▀  ",
+				},
+				{
+					"     ▀▄▀     ▄     ▄",
+					"  ▄███████▄  ▀██▄██▀",
+					"▄█████▀█████▄  ▄█",
+					"███████▀████████▀",
+					" ▄▄▄▄▄▄███████▀",
+				},
+				{
+					"  ▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒",
+					" ▒▐▒▐▒▒▒▒▌▒ ▒▒▌▒▒▐▒▒▌▒",
+					"  ▒▀▄█▒▄▀▒   ▒▀▄▒▌▄▀▒",
+					"     ██         ██",
+					"   ▄▄██▄       ▄██▄   ",
+				},
+				{
+					"  ▄▀▀▀▄ ▄▄      ╠▓    ",
+					"   ▄▀▀▄█▄ ▀▄   ▓╬▓▓▓  ",
+					"  ▀    █ ▀▄   ▓▓╬▓▓▓▓ ",
+					"      ▐▌    ▀▀███████▀",
+					"▒▒▄██████▄▒▒▒▒▒▒▒▒▒▒▒▒",
 				},
 			}
 
-			config.section.header.val = header_style[1]
+      math.randomseed(os.time())
+			config.section.header.val = header_style[math.random(5)]
 			config.section.buttons.val = {
-				button("n", "  New file", "<cmd>ene <CR>"),
-				button("f", "  Find files", [[<cmd>lua require('fzf-lua').files()<cr>]]),
-				button("e", "  Explore", "<cmd>Oil <CR>"),
-				button("o", "  Frecency/MRU", [[<cmd>lua require('fzf-lua').oldfiles()<cr>]]),
-				button("g", "  Grep", [[<cmd>lua require('fzf-lua').live_grep()<cr>]]),
-				button("c", "  Configs", [[<cmd>lua require('fzf-lua').files({ cwd = "~/.config/nvim", })<cr>]]),
-				button("r", "  Open last session", "<cmd>lua require('persistence').load()<cr>"),
+				button("n", "󰝒  New file", "<cmd>ene <CR>"),
+				button("f", "󰱼  Find files", [[<cmd>lua require('fzf-lua').files()<cr>]]),
+				button("e", "󱞊  Explore", "<cmd>Oil <CR>"),
+				button("o", "󰋚  Frecency/MRU", [[<cmd>lua require('fzf-lua').oldfiles()<cr>]]),
+				button("g", "󱎸  Grep", [[<cmd>lua require('fzf-lua').live_grep()<cr>]]),
+				button("c", "󰣖  Configs", [[<cmd>lua require('fzf-lua').files({ cwd = "~/.config/nvim", })<cr>]]),
+				button("r", "󰏒  Open last session", "<cmd>lua require('persistence').load()<cr>"),
 				button("q", "󰩈  Quit", "<cmd>qa!<cr>"),
 			}
 			config.section.footer.val = {
-        "CWD: " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":~") .. "/",
-      }
-      config.section.footer.opts.positon = "center"
+				"CWD: " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":~") .. "/",
+			}
+			config.section.footer.opts.positon = "center"
 
 			require("alpha").setup(config.config)
 		end,
