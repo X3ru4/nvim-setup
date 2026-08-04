6124612d3add80952136c57773e320101cf0940e
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-08-04 18:51:13 +0700

    feat: Add custom command line format and title options to tiny-cmdline plugin.

--- lua/plugins/tiny-cmdline.lua
+++ lua/plugins/tiny-cmdline.lua
@@ -11,6 +11,16 @@ return {
 			title = {
 				enabled = true,
 				pos = 'center',
+				formats = {
+					{ type = ':', pattern = { '^%s*lua%s+', '^%s*lua%s*=', '^%s*=' }, title = ' 󰢱 Lua ' },
+					{ type = ':', pattern = '^%s*!', title = '  Shell ' },
+					{ type = ':', pattern = '^%s*he?l?p?%s+', title = '  Help ' },
+					{ type = ':', pattern = '^%s*color?s?c?h?e?m?e?%s+', title = '  Colorscheme ' },
+					{ type = ':', pattern = '^%s*IncRename%s+', title = '  Rename ' },
+					{ type = '/', title = '  Search ' },
+					{ type = '?', title = '  Search ' },
+					{ title = ' 󰍳 CmdLine ' },
+				},
 			},
 		})
 
