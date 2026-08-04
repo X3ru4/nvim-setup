ee45f6fdb98c192d6a65103eef859001e9c15cd5
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-08-04 18:49:10 +0700

    refactor: Update leap configuration and mappings to use vim.keymap API.

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,50 +1,45 @@
 return {
 	url = 'https://codeberg.org/andyg/leap.nvim',
 	event = 'VeryLazy',
-	keys = {
-		{
-			's',
-			'<Plug>(leap)',
-			mode = { 'n', 'x', 'o' },
-			nowait = true,
-			remap = true,
-		},
-		-- {
-		-- 	"S",
-		-- 	"<Plug>(leap-from-window)",
-		-- 	mode = "n",
-		-- 	nowait = true,
-		-- },
-		{
-			'R',
-			function()
-				require('leap.treesitter').select({
-					opts = require('leap.user').with_traversal_keys('R', 'r'),
-				})
-			end,
-			mode = { 'x', 'o' },
-		},
-	},
-	config = function(_, opts)
-		local leap = require('leap')
-		for k, v in pairs(opts) do
-			leap.opts[k] = v
+	config = function()
+		vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+		vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+		vim.keymap.set({ 'o' }, 'R', '<Plug>(leap-remote-line)')
+		vim.keymap.set({ 'x', 'o' }, 'an', function()
+			require('leap.treesitter').select({
+				opts = require('leap.user').with_traversal_keys('n', 'N'),
+			})
+		end)
+
+		local function ft(kwargs)
+			require('leap').leap(vim.tbl_deep_extend('keep', kwargs, {
+				inputlen = 1,
+				inclusive = true,
+				opts = {
+					-- Force autojump.
+					labels = '',
+					-- Match the modes where you don't need labels (`:h mode()`).
+					safe_labels = vim.fn.mode(1):match('no?') and '' or nil,
+				},
+			}))
 		end
 
-		leap.opts.preview = function(ch0, ch1, ch2)
-			return not (ch1:match('%s') or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a')))
-		end
+		-- A helper function making it easier to set "clever-f" behavior
+		-- (using f/F or t/T instead of ;/, - see the plugin clever-f.vim).
+		local clever = require('leap.user').with_traversal_keys
+		local clever_f, clever_t = clever('f', 'F'), clever('t', 'T')
 
-		leap.opts.equivalence_classes = {
-			' \t\r\n',
-			'([{',
-			')]}',
-			'\'"`',
-		}
-
-		require('leap.user').set_repeat_keys('<enter>', '<backspace>')
-
-		-- vim.keymap.del({ "x", "o" }, "x")
-		-- vim.keymap.del({ "x", "o" }, "X")
+		vim.keymap.set({ 'n', 'x', 'o' }, 'f', function()
+			ft({ opts = clever_f })
+		end)
+		vim.keymap.set({ 'n', 'x', 'o' }, 'F', function()
+			ft({ backward = true, opts = clever_f })
+		end)
+		vim.keymap.set({ 'n', 'x', 'o' }, 't', function()
+			ft({ offset = -1, opts = clever_t })
+		end)
+		vim.keymap.set({ 'n', 'x', 'o' }, 'T', function()
+			ft({ backward = true, offset = 1, opts = clever_t })
+		end)
 	end,
 }
