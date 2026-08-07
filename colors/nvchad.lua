e9307cc60045357ad6bd1df70b2695951975633f
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-08-07 11:05:40 +0700

    feat: Remove nvchad theme and replace with base46.

--- colors/nvchad.lua
+++
@@ -1,3 +0,0 @@
-require('plugins.colorscheme.nvchad').build()
-require('plugins.colorscheme.nvchad').config()
-vim.g.colors_name = 'nvchad'
---
+++ lua/plugins/colorscheme/base46.lua
@@ -0,0 +1,120 @@
+return {
+	'AvengeMedia/base46',
+	name = 'base46',
+	config = function()
+		local base46 = require('base46')
+		base46.setup({
+			-- Each theme has a `type` field set to dark or light.
+			-- If this is true, vim.o.background will be updated accordingly when a theme is loaded.
+			set_background = true,
+			-- If this is true, the 16 colors of neovim's terminal will be set accordingly when a theme is loaded.
+			term_colors = true,
+			-- Enable transparent background.
+			transparency = false,
+
+			-- Contrary to NvChad, all integrations are enabled by default.
+			integrations = {
+				blankline = false,
+				blink = true,
+				cmp = false,
+				defaults = true,
+				devicons = false,
+				git = true,
+				lsp = true,
+				mason = true,
+				neotest = false,
+				nvcheatsheet = false,
+				nvimtree = false,
+				statusline = true,
+				syntax = true,
+				treesitter = true,
+				tbline = false,
+				telescope = false,
+				whichkey = false,
+				alpha = true,
+				avante = false,
+				['blink-pair'] = false,
+				bufferline = false,
+				codeactionmenu = false,
+				dap = false,
+				diffview = false,
+				edgy = false,
+				flash = false,
+				['git-conflict'] = false,
+				gitsigns = false,
+				grug_far = false,
+				hop = false,
+				leap = true,
+				lspsaga = false,
+				markview = false,
+				['mini-tabline'] = true,
+				['mini-icons'] = true,
+				navic = false,
+				neogit = false,
+				notify = false,
+				nvshades = false,
+				orgmode = false,
+				rainbowdelimiters = false,
+				['render-markdown'] = true,
+				semantic_tokens = false,
+				['snacks-dashboard'] = false,
+				['tiny-inline-diagnostic'] = false,
+				todo = true,
+				trouble = false,
+				['vim-illuminate'] = false,
+			},
+		})
+
+		base46.load('gruvbox_light')
+
+		local colors = base46.theme_tables[base46.current_theme]
+		local hl = vimu.highlight
+		hl.apply({
+			basic = {
+				ModeOther = { link = 'St_ConfirmMode' },
+				ModeNormal = { link = 'St_NormalMode' },
+				ModeInsert = { link = 'St_InsertMode' },
+				ModeVisual = { link = 'St_VisualMode' },
+				ModeCommand = { link = 'St_CommandMode' },
+				ModeReplace = { link = 'St_ReplaceMode' },
+
+				FloatTitle = {
+					fg = colors.base_30.black,
+					bg = colors.base_30.blue,
+					bold = true,
+				},
+				FloatFooter = { link = 'FloatTitle' },
+
+				MiniIconsGrey = { fg = colors.base_30.grey },
+				MiniIconsAzure = { fg = colors.base_30.nord_blue },
+				MiniIconsPurple = { fg = colors.base_30.purple },
+				MiniIconsGreen = { fg = colors.base_30.green },
+				MiniIconsBlue = { fg = colors.base_30.blue },
+				MiniIconsCyan = { fg = colors.base_30.cyan },
+				MiniIconsOrange = { fg = colors.base_30.orange },
+				MiniIconsRed = { fg = colors.base_30.red },
+				MiniIconsYellow = { fg = colors.base_30.yellow },
+
+				SnacksIndent = { fg = colors.base_30.line },
+				SnacksIndentScope = { fg = colors.base_30.grey },
+			},
+			extra = {
+				hl.modify('NormalFloat', { fg = colors.base_30.white }),
+
+				-- Syntax
+				hl.modify('Keyword', { italic = true }),
+				hl.modify('@keyword', { italic = true }),
+				hl.modify('@keyword.function', { italic = true }),
+				hl.modify('@keyword.operator', { italic = true }),
+				hl.modify('@keyword.conditional', { italic = true }),
+				hl.modify('@keyword.conditional.ternary', { italic = true }),
+				hl.modify('Function', { bold = true }),
+				hl.modify('@function', { bold = true }),
+				hl.modify('@function.call', { bold = true }),
+				hl.modify('@function.method', { bold = true }),
+				hl.modify('@function.method.call', { bold = true }),
+				hl.modify('Type', { bold = true }),
+			},
+		})
+	end,
+}
