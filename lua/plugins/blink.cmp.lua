067b9a7687afde35986bb39ba4f352b98b91137d
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-08-09 10:30:39 +0700

    fix(refactor blink.cmp config for compatibility with blink.cmp v2):
    Refactored the configuration of the blink completion manager plugin to be compatible with the latest version of blink.cmp.
    
    The changes include:
    - Replaced some dependencies with their newer versions.
    - Introduced a new feature called 'snippet_forward' and 'snippet_backward' for key mappings.
    - Modified the menu to use the 'kind_icon' component and introduced a new function to draw the kind icon.
    - Made some cosmetic changes to make the configuration more consistent and easier to read.
    
    This refactor aims to improve compatibility and usability of the plugin for users who are migrating from an older version of blink.cmp.

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,143 +1,125 @@
 return {
-	'saghen/blink.cmp',
-	event = { 'InsertEnter', 'CmdlineEnter' },
-	version = '1.*',
-	dependencies = {
-		{ 'rafamadriz/friendly-snippets' },
-		{
-			'folke/lazydev.nvim',
-			ft = 'lua',
-			opts = {
-				library = {
-					{ path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-				},
+	{
+		'folke/lazydev.nvim',
+		ft = 'lua',
+		opts = {
+			library = {
+				{ path = '${3rd}/luv/library', words = { 'vim%.uv' } },
 			},
 		},
 	},
-	config = function()
-		local blinkcmp = require('config.extra_options').blinkcmp
+	{
+		-- Using blink.cmp v2
+		'saghen/blink.cmp',
+		build = function()
+			require('blink.cmp').build():pwait()
+		end,
+		dependencies = {
+			'saghen/blink.lib',
+			'rafamadriz/friendly-snippets',
+			'mini-nvim/mini.icons',
+		},
+		event = { 'InsertEnter', 'CmdlineEnter' },
+		config = function()
+			local blinkcmp = require('config.extra_options').blinkcmp
+			local MiniIcons = require('mini.icons')
 
-		require('blink-cmp').setup({
-			keymap = {
-				preset = 'enter',
-				['<Tab>'] = { 'snippet_forward', 'fallback' },
-				['<C-h>'] = { 'snippet_backward', 'fallback' },
-			},
-			cmdline = {
-				enabled = true,
+			require('blink.cmp').setup({
 				keymap = {
-					preset = 'default',
-					['<Tab>'] = { 'show_and_insert_or_accept_single', 'select_next' },
-					['<S-Tab>'] = { 'show_and_insert_or_accept_single', 'select_prev' },
+					preset = 'enter',
+					['<Tab>'] = { 'snippet_forward', 'fallback' },
+					['<C-h>'] = { 'snippet_backward', 'fallback' },
 				},
+				fuzzy = {
+					implementation = 'prefer_rust_with_warning',
+				},
+				cmdline = {
+					enabled = true,
+					keymap = {
+						preset = 'default',
+						['<Tab>'] = { 'show_and_insert_or_accept_single', 'select_next' },
+						['<S-Tab>'] = { 'show_and_insert_or_accept_single', 'select_prev' },
+					},
+					completion = {
+						menu = {
+							auto_show = function()
+								return vim.fn.getcmdtype() == ':'
+							end,
+						},
+					},
+				},
+				appearance = {
+					use_nvim_cmp_as_default = blinkcmp.appearance.use_nvimcmp_hl,
+					nerd_font_variant = 'normal',
+				},
+				signature = { enabled = false },
 				completion = {
+					ghost_text = { enabled = false },
+					accept = { auto_brackets = { enabled = true } },
+					documentation = {
+						auto_show = blinkcmp.documentation.auto_show,
+						auto_show_delay_ms = 500,
+						treesitter_highlighting = true,
+						window = {
+							scrollbar = false,
+							min_width = 30,
+							max_width = 45,
+							max_height = 10,
+							border = blinkcmp.documentation.border,
+						},
+					},
 					menu = {
-						auto_show = function(ctx)
-							return vim.fn.getcmdtype() == ':'
-							-- enable for inputs as well, with:
-							-- or vim.fn.getcmdtype() == '@'
-						end,
-					},
-				},
-			},
-			appearance = {
-				use_nvim_cmp_as_default = blinkcmp.appearance.use_nvimcmp_hl,
-				---@type "mono"|"normal"
-				nerd_font_variant = 'normal',
-				kind_icons = {
-					Text = '',
-					Method = '',
-					Function = '',
-					Constructor = '',
-
-					Field = '',
-					Variable = '',
-					Property = '',
-
-					Class = '',
-					Interface = '',
-					Struct = '',
-					Module = '',
-
-					Unit = '',
-					Value = '',
-					Enum = '',
-					EnumMember = '',
-
-					Keyword = '',
-					Constant = '',
-
-					Snippet = '󰒉',
-					Color = '',
-					File = '',
-					Folder = '',
-					Reference = '',
-					Event = '',
-					Operator = '',
-					TypeParameter = '',
-				},
-			},
-			signature = { enabled = false },
-			completion = {
-				ghost_text = { enabled = false },
-				accept = { auto_brackets = { enabled = true } },
-				documentation = {
-					auto_show = blinkcmp.documentation.auto_show,
-					auto_show_delay_ms = 500,
-					treesitter_highlighting = true,
-					window = {
 						scrollbar = false,
-						min_width = 30,
-						max_width = 45,
-						max_height = 10,
-						border = blinkcmp.documentation.border,
-					},
-				},
-				menu = {
-					scrollbar = false,
-					min_width = 4,
-					max_height = 8,
-					border = blinkcmp.menu.border,
-					draw = {
-						columns = { { 'kind_icon' }, { 'label', 'kind', gap = 1 } },
-						components = {
-							label = {
-								width = { fill = true, max = 30 },
+						min_width = 4,
+						max_height = 8,
+						border = blinkcmp.menu.border,
+						draw = {
+							columns = { { 'kind_icon' }, { 'label', 'kind', gap = 1 } },
+							components = {
+								kind_icon = {
+									text = function(ctx)
+										local kind_icon, _, _ = MiniIcons.get('lsp', ctx.kind)
+										return kind_icon
+									end,
+								},
+								label = {
+									width = { fill = true, max = 30 },
+								},
+								kind = {
+									width = { fill = false },
+								},
 							},
-							kind = {
-								width = { fill = false },
-							},
-						},
-						treesitter = { 'lsp' },
-					},
-				},
-			},
-			sources = {
-				default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
-				providers = {
-					lazydev = {
-						name = 'LazyDev',
-						module = 'lazydev.integrations.blink',
-						enabled = true,
-						score_offset = 100,
-					},
-					lsp = {
-						score_offset = 90,
-					},
-					path = {
-						score_offset = 80,
-					},
-					snippets = {
-						score_offset = 70,
-						opts = {
-							friendly_snippets = true, -- ensure friendly-snippets is enabled
+							treesitter = { 'lsp' },
 						},
 					},
-					buffer = {
-						score_offset = 60,
+				},
+				sources = {
+					default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+					providers = {
+						lazydev = {
+							name = 'LazyDev',
+							module = 'lazydev.integrations.blink',
+							enabled = true,
+							score_offset = 100,
+						},
+						lsp = {
+							score_offset = 90,
+						},
+						path = {
+							score_offset = 80,
+						},
+						snippets = {
+							score_offset = 70,
+							opts = {
+								friendly_snippets = true, -- ensure friendly-snippets is enabled
+							},
+						},
+						buffer = {
+							score_offset = 60,
+						},
 					},
 				},
-			},
-		})
-	end,
+			})
+		end,
+	},
 }
