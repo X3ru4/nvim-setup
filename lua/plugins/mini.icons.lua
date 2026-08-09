5fa178ff5c83dcb9a4ee490333236c15186dcd5b
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-08-09 10:32:37 +0700

    feat(mini.icons): custom lsp snippet icon

--- lua/plugins/mini.icons.lua
+++ lua/plugins/mini.icons.lua
@@ -5,15 +5,12 @@ return {
 	opts = {
 		style = 'glyph',
 		-- Customize per category. See `:h MiniIcons.config` for details.
-		default = {},
 		directory = {
 			Download = { glyph = '󰉍', hl = 'MiniIconsOrange' },
 			Movies = { glyph = '󰎁', hl = 'MiniIconsOrange' },
 		},
-		file = {},
-		filetype = {},
-		extension = {},
-		lsp = {},
-		os = {},
+		lsp = {
+			snippet = { glyph = '󰒉' },
+		},
 	},
 }
