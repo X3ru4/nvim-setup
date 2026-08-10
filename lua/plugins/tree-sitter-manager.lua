cfa6942375808c04a826b8ec5f4d6be003096212
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-08-10 13:07:28 +0700

    refactor(tree-sitter-manager nvim): migrate to opts schema.

--- lua/plugins/tree-sitter-manager.lua
+++ lua/plugins/tree-sitter-manager.lua
@@ -2,21 +2,19 @@ return {
 	'romus204/tree-sitter-manager.nvim',
 	event = 'VeryLazy',
 	dependencies = {}, -- tree-sitter CLI must be installed system-wide
-	config = function()
-		require('tree-sitter-manager').setup({
-			-- list of parsers to install automatically
-			ensure_installed = {
-				'c',
-				'lua',
-				'vim',
-				'vimdoc',
-				'query',
-				'markdown',
-				'markdown_inline',
-			},
-			auto_install = false,
-			nohighlight = {},
-			border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
-		})
-	end,
+	opts = {
+		-- list of parsers to install automatically
+		ensure_installed = {
+			'c',
+			'lua',
+			'vim',
+			'vimdoc',
+			'query',
+			'markdown',
+			'markdown_inline',
+		},
+		auto_install = true,
+		nohighlight = {},
+		border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+	},
 }
