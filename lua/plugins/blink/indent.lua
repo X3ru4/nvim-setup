56effd3ec6afc481163e208d1ab7f405a94bc1e4
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-09-22 14:06:16 +0700

    feat(blink.indent): add <leader>ci keybinding to toggle indent guides

--- lua/plugins/blink/indent.lua
+++ lua/plugins/blink/indent.lua
@@ -1,6 +1,15 @@
 return {
 	'saghen/blink.indent',
 	event = 'BufReadPost',
+	keys = {
+		{
+			'<leader>ci',
+			function()
+				require('blink.indent').enable(not require('blink.indent').is_enabled({ bufnr = 0 }), { bufnr = 0 })
+			end,
+			desc = 'Toggle indent guides',
+		},
+	},
 	--- @module 'blink.indent'
 	--- @type blink.indent.Config
 	opts = {
