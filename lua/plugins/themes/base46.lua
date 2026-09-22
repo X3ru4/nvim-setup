8412dcb445a6714ca426301a90fe0a5a2c131408
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-09-22 15:34:01 +0700

    feat(base46): add MiniIndentscopeSymbol highlight group to theme configuration

--- lua/plugins/themes/base46.lua
+++ lua/plugins/themes/base46.lua
@@ -107,6 +107,7 @@ return {
 
 					BlinkIndent = { fg = colors.base_30.line, bold = true },
 					BlinkIndentScope = { fg = colors.base_30.teal, bold = true },
+					MiniIndentscopeSymbol = { fg = colors.base_30.teal },
 				},
 				extra = {
 					hl.modify('NormalFloat', { fg = colors.base_30.white }),
