c087c5e20f3f4201365e9399b61e159ca0fb905e
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-09-23 12:00:25 +0700

    refactor(fidget): update notification keymaps to use <leader> shortcuts

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -8,21 +8,15 @@ return {
 			progress = {
 				display = {
 					done_icon = '✓',
-					progress_icon = {
-						{
-							'󰸵',
-							'󰸸',
-							'󰸷',
-							'󰸴',
-						},
-						period = 0.5,
-					},
+					progress_icon = { { '󰸵', '󰸸', '󰸷', '󰸴' }, period = 0.5 },
 				},
 			},
 			notification = {
 				override_vim_notify = true,
 			},
 		})
-		vim.keymap.set('n', '<C-h>', fidget.notification.show_history, { desc = 'Show notification history' })
+
+		vim.keymap.set('n', '<leader>hs', fidget.notification.show_history, { desc = 'Show notification history' })
+		vim.keymap.set('n', '<leader>hc', fidget.notification.clear, { desc = 'Clear notification history' })
 	end,
 }
