63f9cb2d7159b1f9075678a752f835ac8bd7e506
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-09-25 20:58:57 +0700

    refactor(usercmds): use vim.cmd.luafile() instead dofile()

--- lua/config/usercmds.lua
+++ lua/config/usercmds.lua
@@ -1,5 +1,6 @@
 local create_cmd = vim.api.nvim_create_user_command
+local path = vim.fn.stdpath('config')
 
 create_cmd('LoadHlConfig', function()
-	dofile(vim.fn.stdpath('config') .. '/lua/config/highlights.lua')
+	vim.cmd.luafile(path .. '/lua/config/highlights.lua')
 end, {})
