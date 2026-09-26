bd33ff3cdfe363372b5f5f96ccec08c963b808b2
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-09-26 07:30:13 +0700

    same

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -245,7 +245,7 @@ return {
 			{
 				flexible = 3,
 				{ provider = [[ %{ mode() =~# '^[ict]$' ? '' : '' } %l∙%c ]] },
-				{ provider = ' %l·%c ' },
+				{ provider = ' %l∙%c ' },
 				{ provider = ' %l ' },
 				false,
 			},
