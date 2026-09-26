0d7f569085c62142a889bae614a6267a6234c6a3
X3ru4 <phammaitrunghuyv3@gmail.com>
2026-09-26 22:04:18 +0700

    feat(usercmds): add bang to LoadHlConfig to disable highlight cache

--- lua/config/usercmds.lua
+++ lua/config/usercmds.lua
@@ -1,6 +1,9 @@
 local create_cmd = vim.api.nvim_create_user_command
 local path = vim.fn.stdpath('config')
 
-create_cmd('LoadHlConfig', function()
+create_cmd('LoadHlConfig', function(opts)
+	if opts.bang then
+		require('utils.highlight').use_cache = false
+	end
 	vim.cmd.luafile(path .. '/lua/config/highlights.lua')
-end, {})
+end, { bang = true })
